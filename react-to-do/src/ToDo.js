bf0a99a53a3c9991b3f920f7b684deddfc07dda2
mkavo92 <mkavo92@gmail.com>
2018-05-09 22:36:47 -0500

    for events assignment

--- react-to-do/src/ToDo.js
+++ react-to-do/src/ToDo.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render() {
     return (
-      <li>
-      <input type="checkbox" checked={ this.props.isCompleted} />
-      <span>{ this.props.description}</span>
-     </li>
+    <li>
+      <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+      <button type="button" onClick={ this.props.deleteTodo } > Delete </button>
+      <span>{ this.props.description }</span>
+    </li>
     );
   }
 }
+
 
 export default ToDo;
